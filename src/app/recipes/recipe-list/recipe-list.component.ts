ede4d36eed0a536eb507081ae47ab264423e3ffc
KalaiSelvan913 <kalaiselvan4913@gmail.com>
2023-11-11 21:44:38 +0530

    add ingredient, shopping module

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit{
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A Ven Pongal Recipe', 'This is simply a Pongal',
+    'https://www.spiceindiaonline.com/wp-content/uploads/2014/01/Ven-Pongal-3.jpg'),
+    new Recipe('A Dosa Recipe', 'This is simply a Dosa',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1w4DlNVS6Teb19NFJr50q-aQFPZpjbHadBg&usqp=CAU')
+   ];
+
+  constructor(){}
 
   ngOnInit(): void {
 
+  }
+
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit{
+  ingredients: Ingredient[] = [
+    new Ingredient('Rice', 1),
+    new Ingredient('Ghee',2)
+  ];
 
+  constructor(){}
+  ngOnInit(): void {
+
+  }
+
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
